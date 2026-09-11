8bcf18392f48a2df4065252a8d6254e631723cdb
Tohadi <toadiasley243@gmail.com>
2026-09-11 16:12:04 +0700

    feat: test tunnel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\CheckRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,9 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
-        $middleware->web(append: [
-            \App\Http\Middleware\ForceHTTPS::class,
-        ]);
+        // $middleware->web(append: [
+        //     ForceHTTPS::class,
+        // ]);
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
